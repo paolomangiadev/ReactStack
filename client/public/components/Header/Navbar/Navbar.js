cbeb06d1799e1cf7a66acc4d931019db4114b4f0
paolomangiadev <paolo.mangia.dev@gmail.com>
2017-03-31 06:55:07 +0200

    menu animation

--- client/public/components/Header/Navbar/Navbar.js
+++ client/public/components/Header/Navbar/Navbar.js
@@ -1,40 +1,45 @@
 import React, { Component } from 'react';
 import css from './Navbar.css';
+import classNames from 'classnames';
+
+const mimmo = 'mimmo';
 
 class Navbar extends Component {
-  componentDidMount() {
+  constructor(props){
+    super(props);
+    this.state = {
+      menuOpen: false
+    }
   }
+
+  initialState(){
+    return this.state.menuOpen;
+  }
+
+  toggleMenu(){
+    console.log(this.state.menuOpen);
+    if (this.state.menuOpen === true) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+
   render(){
     return (
     <div className="hero-head custom-header">
       <header className="nav">
         <div className="container">
-          <div className="nav-left">
-            <a className="nav-item">
+          <div className="toggle-menu">
+            <a className="anchor_wrapper" onClick={this.toggleMenu.bind(this)}>
+            <div className="toggle-menu_wrapper">
+              <span className={classNames('toggle-menu_line', 'toggle-menu_line-first', {openMenu: this.props.menuOpen})}></span>
+              <span className={classNames('toggle-menu_line', 'toggle-menu_line-second', {openMenu: this.props.menuOpen})}></span>
+              <span className={classNames('toggle-menu_line', 'toggle-menu_line-third', {openMenu: this.props.menuOpen})}></span>
+            </div>
             </a>
-          </div>
-          <span className="nav-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-          <div className="nav-right nav-menu">
-            <a className="nav-item is-active">
-              Home
-            </a>
-            <a className="nav-item">
-              Examples
-            </a>
-            <a className="nav-item">
-              Documentation
-            </a>
-            <span className="nav-item">
-              <a className="button is-primary is-inverted">
-                <span className="icon">
-                </span>
-                <span>Download</span>
-              </a>
-            </span>
           </div>
         </div>
       </header>
