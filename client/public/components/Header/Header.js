b73fbc546053c66d14d25cb37ff341d3cf07d908
paolomangiadev <paolo.mangia.dev@gmail.com>
2017-03-31 02:59:56 +0200

    quote section (fully centered)

--- client/public/components/Header/Header.js
+++ client/public/components/Header/Header.js
@@ -3,6 +3,7 @@ import css from './Header.css';
 import Navbar from './Navbar';
 const brand = '../../src/images/netglitch.svg';
 const line = '../../src/images/line.png';
+const line_vertical = '../../src/images/line_vertical.png';
 
 class Header extends Component {
   componentDidMount() {
@@ -15,19 +16,33 @@ class Header extends Component {
 
         <div className="hero-body">
           <div className="container has-text-centered">
-            <span data-wow-duration="0.6s" className="wow fadeInDown we-are">
+            <div data-wow-duration="0.6s" className="wow fadeInDown we-are">
               <div className="line line_pre"><img src={line}/></div>
                 <span>We are</span>
               <div className="line line_post"><img src={line}/></div>
-            </span>
+            </div>
             <span data-wow-duration="0.6s" className="wow fadeIn title brand">
               <img src={brand}/>
             </span>
-            <h2 className="subtitle">
-              <a data-wow-duration="0.6s" className="wow fadeInUp button is-danger is-inverted is-outlined">
-                Get a Quote
-              </a>
-            </h2>
+            <div className="subtitle">
+              <div data-wow-duration="0.6s" className="wow fadeInUp get-quote">
+
+                <div className="get-quote_wrapper left_wrapper">
+                  <p className="item item_left">Services</p>
+
+                  <div className="item item_line item_line_left"></div>
+
+                  <a className="item button is-danger is-inverted is-outlined">
+                    Get a Quote
+                  </a>
+
+                  <div className="item item_line item_line_right"></div>
+
+                  <p className="item item_right">Jobs</p>
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
 
