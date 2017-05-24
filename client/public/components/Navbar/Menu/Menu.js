780b413ebb483a3719e51183f756d63ca61157d2
paolomangiadev <paolo.mangia.dev@gmail.com>
2017-05-25 00:33:32 +0200

    socials fix in burgermenu

--- client/public/components/Navbar/Menu/Menu.js
+++ client/public/components/Navbar/Menu/Menu.js
@@ -3,9 +3,31 @@ import css from './Menu.css';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
+
+let socialList;
 class Menu extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      socials: [
+        {
+          href: 'https://www.facebook.com/Netglitch-639632836220315/',
+          src: '../../../src/images/facebook.png'
+        },
+        {
+          href: 'https://www.instagram.com/netglitch/',
+          src: '../../../src/images/instagram.png'
+        },
+        {
+          href: '#second',
+          src: '../../../src/images/twitter.png'
+        },
+        {
+          href: '#',
+          src: '../../../src/images/linkedin.png'
+        }
+      ]
+    }
   }
 
   toggleMenu() {
@@ -13,6 +35,17 @@ class Menu extends Component {
   }
 
   render(){
+
+    if (this.state.socials) {
+      socialList = _.map(this.state.socials, function(item) {
+        return (
+          <a href={item.href} key={item.src}>
+            <div className="column socials"><img src={item.src}/></div>
+          </a>
+        );
+      });
+    }
+
     return (
     <section className="hero is-fullheight" id="burgerMenu">
       <div className="hero-body">
@@ -20,7 +53,7 @@ class Menu extends Component {
          <nav className="level">
            <div className="level-left">
             <div className="level-item has-text-left socials-level">
-              {this.props.socials}
+              {socialList}
             </div>
           </div>
 
